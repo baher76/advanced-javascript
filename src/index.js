c9045e1ec161c637d2ebc09507ea769892fb92de
Jon <jdonais@stclaircollege.ca>
2018-01-23 18:40:02 -0500

    Minimal react project setup with & babel and a web server to load source files. Execute 'npm start' and navigate to http://localhost:8080 in browser to visit web page.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+ReactDOM.render(
+  <div>
+      <h1>Welcome to JavaScript 5</h1>
+      <span>Example Code</span>
+      <ul>
+          <li>
+            <a href="#">Hello World</a>
+          </li>
+          <li>
+            <HelloWorld/>
+          </li>
+      </ul>
+  </div>,
+  document.getElementById('root')
+);
